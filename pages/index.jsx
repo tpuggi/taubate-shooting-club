503440a04a616e88aa01178e249440a7d19c94da
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-02 22:51:02 -0300

    add: tailwindcss

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,17 @@
-import Image from 'next/image'
-import styles from '../styles/index.module.css';
-import Navbar from '../components/navbar'
+// import Image from 'next/image'
+// import styles from '../styles/index.module.css';
+// import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
-    <main>
-      <Navbar/>
-
+    <main className='bg-black'>
+      {/* <Navbar/> */}
+      aaaaaaaaaaaaaaaaaaaaa
+      <h1 class="text-3xl font-bold underline ">
+        Hello world!
+      </h1>
       {/* <BigCarrousel /> */}
-
+      <button class="bg-blue-500 text-white px-4 py-2 rounded">Clique aqui</button>
       {/* <AboutUs /> */}
 
       {/* <OurServices /> */}
