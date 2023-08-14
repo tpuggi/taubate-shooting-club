9ffd9d76c9bd029ba59597c0ee91a5074e2644df
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-14 03:43:16 -0300

    fix: navbar images

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,27 +1,31 @@
+import logoImage from "../public/images/logoTSC.png";
+import botaoFace from "../public/images/Facebookicon.png";
+import botaoInsta from "../public/images/Instagramicon.png";
 
-import logoImage from '../public/images/logoTSC.png';
-import botaoFace from '../public/images/Facebookicon.png';
-import botaoInsta from '../public/images/Instagramicon.png';
-
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <hr className="border-t" />
-      <nav className="bg-gradient-to-b from-gray-700 to-gray-900 p-4">
+      <nav className="bg-gradient-to-b from-neutral-700 to-neutral-900 p-4">
         <div className="container mx-auto">
-          <div className='flex w-full'>
+          <div className="flex w-full">
             {/* Logo */}
-            <div className="flex justify-center items-center p-5">
-              <Link href="/home">
-                <img src='../public/images/logoTSC.png' className="h-10" />
+            <div className="flex justify-center items-center">
+              <Link href="/">
+                <Image
+                  src={logoImage}
+                  className="h-20 w-full"
+                  alt="Logo Taubaté Shooting Club"
+                  // layout="responsive"
+                />
               </Link>
             </div>
-            <div className='w-full flex flex-col'>
-            {/* Navbar Items */}
-            <div className='pl-20 p-3'>
+            <div className="w-full flex flex-col">
+              {/* Navbar Items */}
+              <div className="pl-20 p-3">
                 <hr className="px-20" />
-            </div>
+              </div>
               <div className="flex justify-around items-center space-x-4 w-full pl-20">
                 <Link href="/home">
                   <p className="text-white">Home</p>
@@ -42,17 +46,18 @@ const Navbar = () => {
                   <p className="text-white">Seja Sócio</p>
                 </Link>
               </div>
-              <div className='pl-20 p-3'>
+              <div className="pl-20 p-3">
                 <hr className="px-20" />
               </div>
             </div>
-            <div className='flex items-center pr-10'>
-                <Link href="/botaoFace">
-                  <img src={botaoFace} className="h-10" />
-                </Link>
-                <Link href="/botaoInsta">
-                  <img src={botaoInsta} className="h-10" />
-                </Link>
+            <div className="flex items-center justify-around">
+              <Link href="/botaoFace">
+                <Image src={botaoFace} className="h-10" />
+              </Link>
+              
+              <Link href="/botaoInsta">
+                <Image src={botaoInsta} className="h-10" />
+              </Link>
             </div>
           </div>
         </div>
@@ -61,4 +66,3 @@ const Navbar = () => {
   );
 };
 export default Navbar;
-
