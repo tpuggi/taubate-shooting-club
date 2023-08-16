51dc6bacde6b0de6f3e1d791b5d16d909b3157da
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-16 18:55:54 -0300

    fix: vercel errors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,20 @@
-
 // import Image from 'next/image'
 // import styles from '../styles/index.module.css';
 // import Navbar from '../components/navbar'
 import Footer from "../components/footer";
-import Navbar from '../components/navbar'
-import Carousel from '../components/bigcarousel'
-import ChamadaCursos from '../components/chamada-cursos'
-
+import Navbar from "../components/navbar";
+import Carousel from "../components/bigcarousel";
+import ChamadaCursos from "../components/chamada-cursos";
 
 export default function Home() {
   return (
     <main className="bg-black">
-      <Navbar/>
-      <Carousel/>
-      <ChamadaCursos/>
+      <Navbar />
 
-      {/* <AboutUs /> */}
+      <Carousel />
 
-      {/* <OurServices /> */}
+      <ChamadaCursos />
 
-      {/* <CTA /> */}
       <Footer />
     </main>
   );
