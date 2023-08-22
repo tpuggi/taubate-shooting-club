0043f213435cae6101475b800fcbf1144d543255
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-21 23:25:53 -0300

    fix: bigcarousel

--- components/bigcarousel.jsx
+++ components/bigcarousel.jsx
@@ -3,9 +3,6 @@ import Image from "next/image";
 
 import { bigCarouselImages } from "../context/context";
 
-import clubePhoto from "../public/images/clube.jpg";
-import clube1Photo from "../public/images/clube1.jpg";
-
 const Carousel = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const totalSlides = bigCarouselImages.length;
@@ -13,53 +10,43 @@ const Carousel = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prevSlide) => (prevSlide + 1) % totalSlides);
-    }, 4000);
+    }, 3000);
 
     return () => clearInterval(interval);
   });
 
-  const nextSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide + 1) % totalSlides);
-  };
-
-  const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide - 1 + totalSlides) % totalSlides);
-  };
-
   return (
-    <div className="flex justify-center mx-25 py-10 overflow-hidden bg-cover">
-      <div className="carousel-container">
+    <div className="carousel">
+      <div className="flex justify-center mx-25 py-10 overflow-hidden bg-cover">
         <div
           className="flex transition-transform duration-300 ease-in-out"
           style={{ transform: `translateX(-${currentSlide * 100}%)` }}
         >
-          {bigCarouselImages.map((image, idx) => {
-            return (
-              <div className="w-full flex-shrink-0 h-[75vh]">
-                <Image
-                  src={image.url}
-                  className="w-full h-[75vh] object-cover"
-                />
-              </div>
-            );
-          })}
+          {bigCarouselImages.map((image, index) => (
+            <div className="w-full flex-shrink-0 h-[75vh]">
+              <Image
+                key={index}
+                src={image.url}
+                alt={`Image ${image.filename}`}
+                className={`w-full h-[75vh] object-cover ${
+                  index === currentSlide ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            </div>
+          ))}
         </div>
       </div>
-
-      <style jsx>
-        {`
-          .carousel-container {
-            display: flex;
-            align-items: center;
-            overflow: hidden;
-          }
-
-          .carousel-slide {
-            width: 100%;
-            height: 100%;
-          }
-        `}
-      </style>
+      <div className="indicators flex justify-center mt-3">
+        {bigCarouselImages.map((_, index) => (
+          <span
+            key={index}
+            className={`w-3 h-3 bg-gray-300 rounded-full mx-1 cursor-pointer ${
+              index === currentSlide ? "bg-gray-700" : ""
+            }`}
+            onClick={() => setCurrentSlide(index)}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 };
