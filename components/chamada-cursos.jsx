7d04cb5a2c1c9f80aaf987da4794d06e62fe2d9b
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-22 22:51:46 -0300

    add: starting carousel in chamada-cursos

--- components/chamada-cursos.jsx
+++ components/chamada-cursos.jsx
@@ -1,8 +1,21 @@
 import bullet from "../public/images/bullete.png";
+import React, { useState, useEffect } from "react";
 
+import { bigCarouselImages } from "../context/context";
 import Image from "next/image";
 
 const ChamadaCursos = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const totalSlides = bigCarouselImages.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % totalSlides);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="flex justify-center items-center bg-red-800 bg-opacity-75">
       <div className="pt-10 text-center justify-center items-center text-white">
@@ -27,6 +40,27 @@ const ChamadaCursos = () => {
           >
             CURSOS
           </button>
+        </div>
+
+        <div className="flex justify-center mx-25 my-10 py-10 overflow-hidden bg-cover">
+          <div
+            className="flex ease-in-out transition-transform duration-300"
+            style={{ transform: `translateX(-${currentSlide * 33}%)` }}
+          >
+            {bigCarouselImages.map((image, index) => (
+              <div
+                key={index}
+                className="flex-none w-1/3 transition-transform transform bg-black translate-x-0 group-hover:translate-x-[33.33%] duration-300 mx-5"
+              >
+                <div class="rounded overflow-hidden shadow-lg">
+                  <Image class="w-full" src={image.url} alt={image.fileName} />
+                  <div class="px-6 py-4 self-center">
+                    <div class="font-bold text-xl mb-2">{image.fileName}</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
