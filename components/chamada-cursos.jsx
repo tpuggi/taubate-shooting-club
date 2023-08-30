b33d887cac4ad3e024aab23558bd403908e2778f
Lukovsk <99260684+Lukovsk@users.noreply.github.com>
2023-08-30 07:32:31 -0300

    add some libs and fix navbar

--- components/chamada-cursos.jsx
+++ components/chamada-cursos.jsx
@@ -27,19 +27,21 @@ const ChamadaCursos = () => {
   };
 
   return (
-    <div className="flex justify-center items-center bg-red-800 bg-opacity-75">
+    <div
+      className="flex justify-center items-center bg-red-800 bg-opacity-75"
+      id="cursos"
+    >
       <div className="pt-10 text-center justify-center items-center text-white">
-        <h1 style={{ fontSize: "80px" }}>Conheça também nossos cursos!</h1>
-        <p style={{ fontSize: "35px" }} className="pr-10 pl-10">
+        <h1 style={{ fontSize: "60px" }}>Conheça também nossos cursos!</h1>
+        <p style={{ fontSize: "25px" }} className="px-10 pb-5">
           A melhor estrutura com stand de tiros, cursos e treinamentos e toda
           assessoria para compra de produtos controlados do Brasil.
-          <br></br>
         </p>
-        <p style={{ fontSize: "50px" }}>AGENDE UMA VISITA AGORA MESMO!</p>
-        <div className="flex justify-center pt-8">
+        <p style={{ fontSize: "35px" }}>AGENDE UMA VISITA AGORA MESMO!</p>
+        <div className="flex justify-center py-4">
           <Image src={bullet} className="w-[89px] h-[22px]" />
         </div>
-        <div className="flex justify-center items-center pt-8">
+        <div className="flex justify-center items-center py-4">
           <button
             className="bg-[#BFAE95] text-center text-red-800 text-3xl pr-20 pl-20 pt-5 pb-5 font-extrabold hover:shadow-2xl"
             style={{
@@ -52,7 +54,7 @@ const ChamadaCursos = () => {
           </button>
         </div>
 
-        <div className="flex justify-center mx-25 my-10 py-10 overflow-hidden bg-cover">
+        <div className="flex justify-center my-5 py-5 overflow-hidden bg-cover">
           <div
             className="flex ease-in-out transition-transform duration-300"
             style={{
--- components/ourservices.jsx
+++ components/ourservices.jsx
@@ -4,196 +4,168 @@ import clube from "../public/images/clube.jpg";
 import Image from "next/image";
 import Link from "next/link";
 
-import {  } from "../context/context";
+import {} from "../context/context";
 
-const titleServices01 = [
-        { text: "Manutenção de Armamentos",},
-]
+const titleServices01 = [{ text: "Manutenção de Armamentos" }];
 const TexServices01 = [
-        { text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",},  
-]
-const imageServices01 = [
-        { src: SomeImage,}
-]
+  {
+    text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",
+  },
+];
+const imageServices01 = [{ src: SomeImage }];
 
-const titleServices02 = [
-        { text: "Manutenção de Armamentos",},
-]
+const titleServices02 = [{ text: "Manutenção de Armamentos" }];
 const TexServices02 = [
-        { text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",},  
-]
-const imageServices02 = [
-        { src: SomeImage,}
-]
+  {
+    text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",
+  },
+];
+const imageServices02 = [{ src: SomeImage }];
 
-const titleServices03 = [
-        { text: "Manutenção de Armamentos",},
-]
+const titleServices03 = [{ text: "Manutenção de Armamentos" }];
 const TexServices03 = [
-        { text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",},  
-]
-const imageServices03 = [
-        { src: SomeImage,}
-]
+  {
+    text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",
+  },
+];
+const imageServices03 = [{ src: SomeImage }];
 
-const titleServices04 = [
-        { text: "Manutenção de Armamentos",},
-]
+const titleServices04 = [{ text: "Manutenção de Armamentos" }];
 const TexServices04 = [
-        { text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",},  
-]
-const imageServices04 = [
-        { src: SomeImage,}
-]
+  {
+    text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",
+  },
+];
+const imageServices04 = [{ src: SomeImage }];
 
-const titleServices05 = [
-        { text: "Manutenção de Armamentos",},
-]
+const titleServices05 = [{ text: "Manutenção de Armamentos" }];
 const TexServices05 = [
-        { text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",},  
-]
-const imageServices05 = [
-        { src: SomeImage,}
-]
+  {
+    text: "O TSC possui armeiro credenciado pela polícia federal capacitado para realizar manutenção preventiva ou corretiva do seu equipamento. Também realizamos customização do seu armamento. Pintura e gravação à Laser são nossas especialidades, pelo convênio com a Cammo Arts Custom.",
+  },
+];
+const imageServices05 = [{ src: SomeImage }];
 
 const OurServices = () => {
-    return (
-        <div className="text-white mt-32">
-                <p className="text-[80px] neue_montreal-font pl-10">
-                Nossos Serviços
-                </p>
-                <p className="text-[25px] pl-10 pt-5">
-                Realizamos diversos <strong>cursos de capacitação</strong> com convênio para aplicar os <br></br>testes exigidos para a <strong>aquisição</strong> de armas de fogo, <strong>renovação</strong> de registros <br></br>ou <strong>expedição</strong> de Certificado de Registro de CAC (Colecionador - Atirador ou <br></br> Caçador).
-                </p>
-                <div className="grid grid-cols-2 gap-3 pb-20">
-                        <div className="mr-2 pl-10 pt-10">
-                        {imageServices01.map((imageServices01, idx) => (
-                                  <Image
-                                    key={idx}
-                                    src={imageServices01.src}
-                                    className="w-full h-full mr-2"
-                                  />
-                              ))}
-                        </div>
-                        <div className="ml-2 pr-10 pt-10">
-                                <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
-                                        {titleServices01.map((titleServices01, idx) => (
-                                                <p
-                                                className="text-[32px]"
-                                                >{titleServices01.text}</p>
-                                                ))}
-                                </div>
-                                <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
-                                        {TexServices01.map((TexServices01, idx) => (
-                                                <p
-                                                className="text-[22px]"
-                                                >{TexServices01.text}</p>
-                                                ))}
-                                </div>
-                        </div>
-                        <div className="ml-2 pl-10 pt-10">
-                                <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
-                                {titleServices02.map((titleServices02, idx) => (
-                                                <p
-                                                className="text-[32px]"
-                                                >{titleServices02.text}</p>
-                                                ))}
-                                </div>
-                                <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
-                                {TexServices02.map((TexServices02, idx) => (
-                                                <p
-                                                className="text-[22px]"
-                                                >{TexServices02.text}</p>
-                                                ))}
-                                </div>
-                        </div>
-                        <div className="mr-2 pr-10 pt-10">
-                        {imageServices02.map((imageServices02, idx) => (
-                                  <Image
-                                    key={idx}
-                                    src={imageServices02.src}
-                                    className="w-full h-full mr-2"
-                                  />
-                              ))}
-                        </div>
-                        <div className="mr-2 pl-10 pt-10">
-                        {imageServices03.map((imageServices03, idx) => (
-                                  <Image
-                                    key={idx}
-                                    src={imageServices03.src}
-                                    className="w-full h-full mr-2"
-                                  />
-                              ))}
-                        </div>
-                        <div className="ml-2 pr-10 pt-10">
-                                <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
-                                {titleServices03.map((titleServices03, idx) => (
-                                                <p
-                                                className="text-[32px]"
-                                                >{titleServices03.text}</p>
-                                                ))}
-                                </div>
-                                <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
-                                {TexServices03.map((TexServices03, idx) => (
-                                                <p
-                                                className="text-[22px]"
-                                                >{TexServices03.text}</p>
-                                                ))}
-                                </div>
-                        </div>
-                        <div className="ml-2 pl-10 pt-10">
-                                <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
-                                {titleServices04.map((titleServices04, idx) => (
-                                                <p
-                                                className="text-[32px]"
-                                                >{titleServices04.text}</p>
-                                                ))}
-                                </div>
-                                <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
-                                {TexServices04.map((TexServices04, idx) => (
-                                                <p
-                                                className="text-[22px]"
-                                                >{TexServices04.text}</p>
-                                                ))}
-                                </div>
-                        </div>
-                        <div className="mr-2 pr-10 pt-10">
-                        {imageServices04.map((imageServices04, idx) => (
-                                  <Image
-                                    key={idx}
-                                    src={imageServices04.src}
-                                    className="w-full h-full mr-2"
-                                  />
-                              ))}
-                        </div>
-                        <div className="mr-2 pl-10 pt-10">
-                        {imageServices05.map((imageServices05, idx) => (
-                                  <Image
-                                    key={idx}
-                                    src={imageServices05.src}
-                                    className="w-full h-full mr-2"
-                                  />
-                              ))}
-                        </div>
-                        <div className="ml-2 pr-10 pt-10">
-                                <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
-                                {titleServices05.map((titleServices05, idx) => (
-                                                <p
-                                                className="text-[32px]"
-                                                >{titleServices05.text}</p>
-                                                ))}
-                                </div>
-                                <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
-                                {TexServices05.map((TexServices05, idx) => (
-                                                <p
-                                                className="text-[22px]"
-                                                >{TexServices05.text}</p>
-                                                ))}
-                                </div>
-                        </div>
-                </div>
+  return (
+    <div className="text-white mt-32" id="servicos">
+      <p className="text-[80px] neue_montreal-font pl-10">Nossos Serviços</p>
+      <p className="text-[25px] pl-10 pt-5">
+        Realizamos diversos <strong>cursos de capacitação</strong> com convênio
+        para aplicar os <br></br>testes exigidos para a{" "}
+        <strong>aquisição</strong> de armas de fogo, <strong>renovação</strong>{" "}
+        de registros <br></br>ou <strong>expedição</strong> de Certificado de
+        Registro de CAC (Colecionador - Atirador ou <br></br> Caçador).
+      </p>
+      <div className="grid grid-cols-2 gap-3 pb-20">
+        <div className="mr-2 pl-10 pt-10">
+          {imageServices01.map((imageServices01, idx) => (
+            <Image
+              key={idx}
+              src={imageServices01.src}
+              className="w-full h-full mr-2"
+            />
+          ))}
         </div>
-    );
+        <div className="ml-2 pr-10 pt-10">
+          <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
+            {titleServices01.map((titleServices01, idx) => (
+              <p className="text-[32px]">{titleServices01.text}</p>
+            ))}
+          </div>
+          <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
+            {TexServices01.map((TexServices01, idx) => (
+              <p className="text-[22px]">{TexServices01.text}</p>
+            ))}
+          </div>
+        </div>
+        <div className="ml-2 pl-10 pt-10">
+          <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
+            {titleServices02.map((titleServices02, idx) => (
+              <p className="text-[32px]">{titleServices02.text}</p>
+            ))}
+          </div>
+          <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
+            {TexServices02.map((TexServices02, idx) => (
+              <p className="text-[22px]">{TexServices02.text}</p>
+            ))}
+          </div>
+        </div>
+        <div className="mr-2 pr-10 pt-10">
+          {imageServices02.map((imageServices02, idx) => (
+            <Image
+              key={idx}
+              src={imageServices02.src}
+              className="w-full h-full mr-2"
+            />
+          ))}
+        </div>
+        <div className="mr-2 pl-10 pt-10">
+          {imageServices03.map((imageServices03, idx) => (
+            <Image
+              key={idx}
+              src={imageServices03.src}
+              className="w-full h-full mr-2"
+            />
+          ))}
+        </div>
+        <div className="ml-2 pr-10 pt-10">
+          <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
+            {titleServices03.map((titleServices03, idx) => (
+              <p className="text-[32px]">{titleServices03.text}</p>
+            ))}
+          </div>
+          <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
+            {TexServices03.map((TexServices03, idx) => (
+              <p className="text-[22px]">{TexServices03.text}</p>
+            ))}
+          </div>
+        </div>
+        <div className="ml-2 pl-10 pt-10">
+          <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
+            {titleServices04.map((titleServices04, idx) => (
+              <p className="text-[32px]">{titleServices04.text}</p>
+            ))}
+          </div>
+          <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
+            {TexServices04.map((TexServices04, idx) => (
+              <p className="text-[22px]">{TexServices04.text}</p>
+            ))}
+          </div>
+        </div>
+        <div className="mr-2 pr-10 pt-10">
+          {imageServices04.map((imageServices04, idx) => (
+            <Image
+              key={idx}
+              src={imageServices04.src}
+              className="w-full h-full mr-2"
+            />
+          ))}
+        </div>
+        <div className="mr-2 pl-10 pt-10">
+          {imageServices05.map((imageServices05, idx) => (
+            <Image
+              key={idx}
+              src={imageServices05.src}
+              className="w-full h-full mr-2"
+            />
+          ))}
+        </div>
+        <div className="ml-2 pr-10 pt-10">
+          <div className="origin-top-left bg-gradient-to-b from-zinc-800 to-neutral-900 rounded-tl-[10px] rounded-tr-[10px] pl-3 pt-3 pb-3">
+            {titleServices05.map((titleServices05, idx) => (
+              <p className="text-[32px]">{titleServices05.text}</p>
+            ))}
+          </div>
+          <div className="origin-top-left bg-neutral-900 mt-1 p-10 pb-28">
+            {TexServices05.map((TexServices05, idx) => (
+              <p className="text-[22px]">{TexServices05.text}</p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurServices;
